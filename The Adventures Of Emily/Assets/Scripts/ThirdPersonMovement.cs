8238051903182b93c3fb6776a39bc31582fde5d4
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2022-01-02 12:59:36 +0300

    Soaring mechanic and improved double jump

--- The Adventures Of Emily/Assets/Scripts/ThirdPersonMovement.cs
+++ The Adventures Of Emily/Assets/Scripts/ThirdPersonMovement.cs
@@ -18,18 +18,25 @@ public class ThirdPersonMovement : MonoBehaviour
     private float jumpForce = 10f;
     float turnSmoothVelocity;
     float vSpeed = 0;
-    private bool doubleJumped = false;
+    private bool secondJump = false;
+    private bool firstJump = false;
     private bool previousGroundedStatus = true;
+    [SerializeField]
+    private float gravityMultiplier = 0.5f;
+    private float gravity = 9.81f;
+
+    [SerializeField]
+    private float soarSpeed = 1f;
 
     public Transform cam;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        OnCharacterLandedAfterDoubleJump += () => 
-        { 
+        OnCharacterLandedAfterDoubleJump += () =>
+        {
             Debug.Log("Landed after double jump!");
-            doubleJumped = false;
+            secondJump = false;
         };
     }
 
@@ -50,50 +57,60 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-            if (direction.magnitude <= 0.1f)
+        if (direction.magnitude <= 0.1f)
+        {
+            moveDir = Vector3.zero;
+        }
+
+        if (characterController.isGrounded)
+        {
+            //vSpeed = -0.01f;
+            print("character grounderd");
+            firstJump = false;
+        }
+        else
+        {
+            if(Input.GetKey(KeyCode.Space) && vSpeed <= 0)
             {
-                moveDir = Vector3.zero;
-            }
-
-            if (characterController.isGrounded)
-            {
-                vSpeed = -0.01f;
+                vSpeed = -soarSpeed;
             }
             else
             {
-                vSpeed -= 9.81f * Time.deltaTime * 0.5f;
+                vSpeed -= gravity * gravityMultiplier;
+                print("character not grounded");
             }
+        }
 
 
-            if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!firstJump && !secondJump)
             {
-                if (characterController.isGrounded && !doubleJumped)
-                {
-                    vSpeed = jumpForce;
-                }
-                else if (!characterController.isGrounded && !doubleJumped)
-                {
-                    vSpeed = jumpForce;
-                    doubleJumped = true;
-                }
+                print("fisrt jump");
+                vSpeed = jumpForce;
+                firstJump = true;
             }
+            else if (firstJump && !secondJump)
+            {
+                print("second jump");
+                vSpeed = jumpForce;
+                secondJump = true;
+            }
+        }
 
-            moveDir.y = vSpeed;
-            characterController.Move(moveDir * speed * Time.deltaTime);
-        
-
-        Debug.Log(vSpeed);
+        moveDir.y = vSpeed;
+        characterController.Move(moveDir * speed * Time.deltaTime);
 
 
 
 
-        if(!previousGroundedStatus && doubleJumped && characterController.isGrounded)
+        if (!previousGroundedStatus && secondJump && characterController.isGrounded)
         {
             OnCharacterLandedAfterDoubleJump?.Invoke();
         }
