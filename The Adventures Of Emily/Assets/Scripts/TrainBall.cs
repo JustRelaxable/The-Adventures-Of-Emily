9f6363ef51ca1b6ad0fd43f1dff6de10129da79e
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2022-04-05 21:39:46 +0300

    Last updates before prototype

--- The Adventures Of Emily/Assets/Scripts/TrainBall.cs
+++ The Adventures Of Emily/Assets/Scripts/TrainBall.cs
@@ -6,12 +6,29 @@ public class TrainBall : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject spawnPoint;
+    public AudioClip clip;
+    private AudioSource audioSource;
+    private SphereCollider sphereCollider;
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        sphereCollider = GetComponent<SphereCollider>();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Projectile") || collision.gameObject.CompareTag("Train"))
         {
             Instantiate(explosion).transform.position = spawnPoint.transform.position;
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
+            sphereCollider.enabled = false;
+            meshRenderer.enabled = false;
+            audioSource.PlayOneShot(clip);
         }
     }
 }
