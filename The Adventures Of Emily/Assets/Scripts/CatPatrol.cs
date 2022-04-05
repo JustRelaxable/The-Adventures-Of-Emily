fad476221850d1d03c688fffbc0ec23eb63e7713
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2022-04-05 11:31:38 +0300

    Cat patrol improved

--- The Adventures Of Emily/Assets/Scripts/CatPatrol.cs
+++ The Adventures Of Emily/Assets/Scripts/CatPatrol.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatPatrol : MonoBehaviour
 {
@@ -8,23 +10,43 @@ public class CatPatrol : MonoBehaviour
     public GameObject character;
     public GameObject catRestPoint;
     private Coroutine currentCoroutine;
+    private NavMeshAgent navMeshAgent;
+    private Action followAction;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
 
     private void Start()
     {
+        //navMeshAgent.SetDestination(character.transform.position);
+    }
+
+    private void Update()
+    {
+        followAction?.Invoke();
     }
 
     public void GoToCharacter()
-    {
+    {/*
         if(currentCoroutine!=null)
             StopCoroutine(currentCoroutine);
         currentCoroutine = StartCoroutine(GoTo(character.transform));
+        */
+        followAction += () => navMeshAgent.SetDestination(character.transform.position);
     }
 
     public void GoToRest()
     {
+        /*
         if (currentCoroutine != null)
             StopCoroutine(currentCoroutine);
         currentCoroutine = StartCoroutine(GoTo(catRestPoint.transform));
+        */
+        followAction = () => { };
+        navMeshAgent.SetDestination(catRestPoint.transform.position);
     }
 
     IEnumerator GoTo(Transform t)
