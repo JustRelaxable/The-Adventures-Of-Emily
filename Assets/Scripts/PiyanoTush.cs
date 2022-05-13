2f55ead1d0443792b590d7e9ba335fdae924d79f
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2022-05-14 01:06:43 +0300

    Piyano Tuş animasyonları eklendi

---
+++ Assets/Scripts/PiyanoTush.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiyanoTush : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetTrigger("Pushed");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetTrigger("NotPushed");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Xd");
+    }
+}
