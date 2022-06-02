5c6d5748c61740f36bb9bdcc6e25dbdb59960f19
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2022-06-03 02:12:46 +0300

    Level connections

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    [SerializeField] int nextLevelIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextLevelIndex);
+        }
+    }
+}
