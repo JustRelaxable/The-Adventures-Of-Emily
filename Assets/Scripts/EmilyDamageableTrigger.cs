9a6c83d3f4da2fba314d613bbb5392d1e6258774
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2022-06-27 12:29:08 +0300

    Final level fall damage added

---
+++ Assets/Scripts/EmilyDamageableTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmilyDamageableTrigger : MonoBehaviour
+{
+    [SerializeField] Transform teleportTransform;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            EmilyHealthController emilyHealthController = other.GetComponent<EmilyHealthController>();
+            emilyHealthController.DealDamageToEmily(1);
+            if(emilyHealthController.GetCurrentHealth() <= 0)
+            {
+                //Don'T teleport
+            }
+            else
+            {
+                other.gameObject.transform.position = teleportTransform.position;
+            }
+        }
+    }
+}
