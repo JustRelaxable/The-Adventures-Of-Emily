81605e83141ae0aea30aa0801c93cb0d287a60d3
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2022-06-27 18:54:07 +0300

    Golf scene rolling ball implemented

--- Assets/Scripts/CardMan.cs
+++ Assets/Scripts/CardMan.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardMan : MonoBehaviour,IAttackable
+public class CardMan : MonoBehaviour,IAttackable,IHitPoints
 {
     public float radius;
     public float speed;
     public float attackRadius;
     [SerializeField] int hitPoints;
     private Animator animator;
-    
+
+    public int HitPoints => hitPoints;
 
     private void Awake()
     {
